1388504192f35665d191ca96c94ccae588e92e6b
AbhishekPattnaik04 <58695787+AbhishekPattnaik04@users.noreply.github.com>
2020-05-21 13:45:08 +0530

    Update pythonscript.py

--- infra/pythonscript.py
+++ infra/pythonscript.py
@@ -1,6 +1,6 @@
 from python_terraform import *
 
-tf = Terraform(working_dir='/root/infraprovision/infra', variables={'count':enter})
+tf = Terraform(working_dir='/var/lib/jenkins/workspace/Python-Terraform/infra', variables={'count':enter})
 tf.plan(no_color=IsFlagged, refresh=False, capture_output=True)
 approve = {"auto-approve": True}
 print(tf.init(reconfigure=True))
